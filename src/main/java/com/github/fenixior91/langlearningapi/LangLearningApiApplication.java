11db507eee4258a05d65c14514cf2ff0391d543b
Tomasz Truszkowski <harpoon2009@gmail.com>
2018-03-18 15:56:05 +0100

    [0.1][API]Spring Boot initial commit.

---
+++ src/main/java/com/github/fenixior91/langlearningapi/LangLearningApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.fenixior91.langlearningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangLearningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangLearningApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/fenixior91/langlearningapi/LangLearningApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.fenixior91.langlearningapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LangLearningApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
